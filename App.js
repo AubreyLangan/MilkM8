773f83f9866e07a72d63af8255405aa4e02ca0fe
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-11-21 17:13:30 -0800

    Commit 2 21 Nov 24
    Updates to the delete button to make it function better
    Added undo button function

--- App.js
+++ App.js
@@ -6,12 +6,25 @@ import './App.css';
 
 function App() {
   const [entries, setEntries] = useState([]);
+  const [deletedEntry, setDeletedEntry] = useState(null);
 
   const addEntry = (newEntry) => setEntries([newEntry, ...entries]);
 
   const deleteEntry = (id) => {
-    setEntries((prevEntries) => prevEntries.filter((entry) => entry.id !== id));
-  }
+    const entryToDelete = entries.find((entry) => entry.id === id);
+    if (window.confirm("Are you sure you want to delete this entry?")) {
+      setDeletedEntry(entryToDelete);
+      setEntries((prevEntries) => prevEntries.filter((entry) => entry.id !== id));
+    }
+  };
+
+  const undoDelete = () => {
+    if (deletedEntry) {
+      setEntries((prevEntries) => [...prevEntries, deleteEntry]);
+      setDeletedEntry(null);
+    }
+  };
+
   return (
     <div className="container">
       <header>
@@ -21,6 +34,22 @@ function App() {
       <LogEntryForm addEntry={addEntry} />
       <StatsSummary entries={entries} />
       <LogEntryList entries={entries} deleteEntry={deleteEntry}/>
+      {deletedEntry && (
+        <button 
+          onClick={undoDelete} 
+          style={{ 
+            marginTop: '10px', 
+            background: 'green', 
+            color: 'white', 
+            padding: '10px',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer', 
+          }}
+        >
+          Undo Last Delete
+        </button>
+      )}
     </div>
   );
 }
