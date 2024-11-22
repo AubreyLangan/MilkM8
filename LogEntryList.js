560068214a2336c7e61e0d1e60fb4bce58e67498
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-11-21 17:09:46 -0800

    Commit 21 Nov 24
    Added delete button functionality

--- LogEntryList.js
+++ LogEntryList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const LogEntryList = ({ entries }) => (
+const LogEntryList = ({ entries, deleteEntry }) => (
     <div>
         <h2>Pumping Log</h2>
         <ul>
@@ -8,8 +8,9 @@ const LogEntryList = ({ entries }) => (
                 <li key={entry.id}>
                     <p>Date: {entry.date}</p>
                     <p>Time: {entry.time}</p>
-                    <p>Quantity: {entry.quantity}</p>
+                    <p>Quantity: {entry.quantity} {entry.unit}</p>
                     <p>Notes: {entry.notes}</p>
+                    <button onClick={() => deleteEntry(entry.id)}>Delete</button>
                 </li>
             ))}
         </ul>
