4f4fe79e91a370f858ce152fe202e80fc2c61d87
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-11-22 11:32:38 -0800

    Commit 22 Nov 24
    Addition of routes and links

--- App.js
+++ App.js
@@ -1,57 +1,34 @@
 import React, { useState } from "react";
-import LogEntryForm from "./Components/LogEntryForm";
-import LogEntryList from "./Components/LogEntryList";
-import StatsSummary from "./Components/StatsSummary";
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Pages/Home';
+import LogEntry from './Pages/LogEntry';
+import Stats from './Pages/Stats';
 import './App.css';
 
-function App() {
+const App = () => {
   const [entries, setEntries] = useState([]);
-  const [deletedEntry, setDeletedEntry] = useState(null);
 
-  const addEntry = (newEntry) => setEntries([newEntry, ...entries]);
-
-  const deleteEntry = (id) => {
-    const entryToDelete = entries.find((entry) => entry.id === id);
-    if (window.confirm("Are you sure you want to delete this entry?")) {
-      setDeletedEntry(entryToDelete);
-      setEntries((prevEntries) => prevEntries.filter((entry) => entry.id !== id));
-    }
-  };
-
-  const undoDelete = () => {
-    if (deletedEntry) {
-      setEntries((prevEntries) => [...prevEntries, deleteEntry]);
-      setDeletedEntry(null);
-    }
+  const addEntry = (entry) => {
+    setEntries((prevEntries) => [...prevEntries, entry]);
   };
 
   return (
-    <div className="container">
-      <header>
-        <h1>MilkMate</h1>
-        <p>Your personal breastmilk tracking companion</p>
-      </header>
-      <LogEntryForm addEntry={addEntry} />
-      <StatsSummary entries={entries} />
-      <LogEntryList entries={entries} deleteEntry={deleteEntry}/>
-      {deletedEntry && (
-        <button 
-          onClick={undoDelete} 
-          style={{ 
-            marginTop: '10px', 
-            background: 'green', 
-            color: 'white', 
-            padding: '10px',
-            border: 'none',
-            borderRadius: '5px',
-            cursor: 'pointer', 
-          }}
-        >
-          Undo Last Delete
-        </button>
-      )}
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/log-entry">Log Entry</Link></li>
+          <li><Link to="/stats">Stats</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/log-entry" element={<LogEntry addEntry={addEntry} />} />
+        <Route path="/stats" element={<Stats entries={entries} />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
