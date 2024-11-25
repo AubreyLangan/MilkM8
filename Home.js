da765e4eb300c1699a8d2d44185ee42374fc8428
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-11-25 14:06:29 -0800

    Commit 25 Nov 24
    Slight change to name of app

--- Home.js
+++ Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div>
-            <h1>Welcome to MilkMate</h1>
+            <h1>Welcome to MilkM8</h1>
             <p>Track and log your breastmilk efficiently!</p>
         </div>
     );
