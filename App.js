ea35eed0738a775560aa614184fba58b5e33c0b8
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-12-03 13:05:28 -0800

    Commit 3 Dec 24
    Update to include new edit button that was created

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar";
 import Home from './Pages/Home';
 import LogEntry from './Pages/LogEntry';
 import Stats from './Pages/Stats';
+import LogEntries from "./Components/LogEntries";
 import './App.css';
 
 const App = () => {
@@ -16,15 +17,24 @@ const App = () => {
   const deleteEntry = (id) => {
     setEntries((prevEntries) => prevEntries.filter((entry) => entry.id !== id));
   }
+  
+  const updateEntry = (updatedEntry) => {
+    setEntries((prevEntries) =>
+      prevEntries.map((entry) =>
+        entry.id === updatedEntry.id ? updatedEntry : entry
+      )
+    );
+  }; 
 
   return (
       <div className="app">
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/log-entry" element={<LogEntry addEntry={addEntry} entries={entries} deleteEntry={deleteEntry}/>} />
+            <Route path="/" element={<Home entries={entries} />} />
+            <Route path="/log-entry" element={<LogEntry addEntry={addEntry} entries={entries} deleteEntry={deleteEntry} updateEntry={updateEntry} />} />
             <Route path="/stats" element={<Stats entries={entries} />} />
+            <Route path="log-entries" element={<LogEntries entries={entries} updateEntry={updateEntry}  setEntries={setEntries} />} />
           </Routes>
         </div>
       </div>
