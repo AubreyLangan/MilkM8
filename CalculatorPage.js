b65c604aeb99ca3b76cab6cd1cef36b17737ee1e
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-12-13 12:01:50 -0800

    Commit 13 Dec 24
    Updated code to include new calculators

--- CalculatorPage.js
+++ CalculatorPage.js
@@ -1,11 +1,29 @@
 import React from "react";
-import MilkStashCalculator from "../Components/Calculator";
+import MilkStashCalculator from "../Components/Calculators/Calculator";
+import DepletionCalculator from "../Components/Calculators/DepletionCalculator";
+import TargetGoalCalculator from "../Components/Calculators/TargetGoalCalculator";
+import EventMilkCalculator from "../Components/Calculators/EventMilkCalculator";
 
 const CalculatorPage = () => {
     return (
         <div className="calculator-page">
-            <h1>Milk Stash Calculators</h1>
-            <MilkStashCalculator />
+            <h1>Milk Calculators</h1>
+            <div className="calculator-section">
+                <h2>Days until Depletion</h2>
+                <DepletionCalculator />
+            </div>
+            <div className="calculator-section">
+                <h2>Target Goal Calculator</h2>
+                <TargetGoalCalculator />
+            </div>
+            <div className="calculator-section">
+                <h2>Milk Stash Calculator</h2>
+                <MilkStashCalculator />
+            </div>
+            <div className="calculator-section">
+                <h2>Event Milk Calculator</h2>
+                <EventMilkCalculator />
+            </div>
         </div>
     );
 };
