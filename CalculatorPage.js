2626f7bb2c205549b8654ea7678a8a7a6f011d2f
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-12-12 16:57:37 -0800

    Initial Commit 12 Dec 24
    new page

---
+++ CalculatorPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MilkStashCalculator from "../Components/Calculator";
+
+const CalculatorPage = () => {
+    return (
+        <div className="calculator-page">
+            <h1>Milk Stash Calculators</h1>
+            <MilkStashCalculator />
+        </div>
+    );
+};
+
+export default CalculatorPage;
