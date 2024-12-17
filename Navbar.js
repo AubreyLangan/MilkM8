cc40be2416da3db029c19bceebea4f5ae1ed6034
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-12-16 16:33:56 -0800

    Commit 16 Dec 24

--- Navbar.js
+++ Navbar.js
@@ -2,19 +2,18 @@ import React, { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import BackButton from "./BackButton";
 import logoidea from "../Assets/logoidea2.PNG";
+import { useTheme } from "../utils/ThemeContext";
 import './Navbar.css'
 
-const Navbar = ({ toggleTheme, theme }) => {
+const Navbar = () => {
+    const { isDarkMode, toggleTheme } = useTheme();
     const location = useLocation();
     const isHome = location.pathname === "/";
 
     const [isOpen, setIsOpen] = useState(false);
 
     const handleToggle = () => {
-        setIsOpen((prevState) => {
-            console.log("Toggling Navbar:", !prevState);
-            return !prevState;
-        });
+        setIsOpen((prevState) => !prevState);
     };
 
     return (
@@ -31,6 +30,10 @@ const Navbar = ({ toggleTheme, theme }) => {
             </div>
 
             {!isHome && <BackButton />}
+
+            <button className="theme-toggle" onClick={toggleTheme}>
+                {isDarkMode ? "Light Mode" : "Dark Mode"}
+            </button>
         </div>
         <ul className={`menu ${isOpen ? "open" : ""}`} data-isopen={isOpen}>
             <li>
