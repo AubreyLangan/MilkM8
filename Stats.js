7dae18b2097b286da1b6470399769b2dc4648800
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-12-18 12:18:17 -0800

    Commit 18 Dec 24
    Code added to function with the new dark mode feature

--- Stats.js
+++ Stats.js
@@ -1,10 +1,19 @@
 import React from "react";
 import StatsSummary from '../Components/StatsSummary';
+import { useTheme } from "../utils/ThemeContext";
 
 const Stats = ({ entries }) => {
+    const totalMilk = entries.reduce((total, entry) => {
+        const quantity = parseFloat(entry.quantity);
+        return total + (isNaN(quantity) ? 0 : quantity);
+    }, 0);
+    const { isDarkMode } = useTheme();
+
+
     return (
-        <div>
+        <div className={`stats ${isDarkMode ? "dark" : "light"}`}>
             <h1>Statistics</h1>
+            <p>Total Milk Logged: {totalMilk.toFixed(2)} {entries.length > 0 ? entries[0].unit : 'oz'}</p>
             <StatsSummary entries={entries} />
         </div>
     );
