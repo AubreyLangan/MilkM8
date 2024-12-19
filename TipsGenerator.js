15dfbfe6ed1d0e2389387def73655db79d31a706
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-12-18 20:24:47 -0800

    Commit 18 Dec 24
    Addition of a new button to improve functionality

--- TipsGenerator.js
+++ TipsGenerator.js
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
 import { breastfeedingTips } from "../Data/Tips";
+import { useNavigate } from "react-router-dom";
 
 const TipsGenerator = () => {
     const [currentTip, setCurrentTip] = useState("");
+    const navigate = useNavigate();
 
     const generateTip = () => {
         const randomeIndex = Math.floor(Math.random() * breastfeedingTips.length);
         setCurrentTip(breastfeedingTips[randomeIndex]);
     };
 
+    const goToResources = () => {
+        navigate("/resources")
+    }
+
     return (
         <div className="tips-generator">
             <h2>Breastfeeding Tips</h2>
             <p>{currentTip || "Click below to get a tip!"}</p>
             <button onClick={generateTip}>Generate Tip</button>
+            <button onClick={goToResources} className="resource-button">
+                Go to Resources
+            </button>
         </div>
     );
 };
