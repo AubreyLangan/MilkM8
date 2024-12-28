0716ec931129b159dbaf608c48230c54d5f43f0a
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2024-12-27 16:12:26 -0800

    Initial Commit 27 Dec 24
    Initial coding for the new Footer component

---
+++ Footer.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+    return (
+        <footer className="app-footer">
+            <div className="footer-content">
+                <div className="footer-section about">
+                    <h3>About</h3>
+                    <p>This app helps you track feeding, pumping, and more to simplify baby care routines.</p>
+                </div>
+                <div className="footer-section links">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li><Link to="/help-center">Help Center</Link></li>
+                        <li><Link to="/privacy-policy">Privacy Policy</Link></li>
+                        <li><Link to="/terms-of-service">Terms of Service</Link></li>
+                    </ul>
+                </div>
+                <div className="footer-section contact">
+                    <h3>Contact Us</h3>
+                    <p>Email:</p>
+                    <div className="social-media">
+                        <a href=""></a>
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; 2024 MilkM8. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
