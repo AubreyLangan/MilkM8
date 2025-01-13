df6f44721c2b5c796f1b5c44397a3243043e3b56
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2025-01-13 13:20:33 -0800

    Commit 13 Jan 25
    Small change made to resolve an error

--- FeedbackForm.js
+++ FeedbackForm.js
@@ -41,7 +41,7 @@ const FeedbackForm = () => {
                         <input
                             type="text"
                             value={name}
-                            onChange={(e) => setName(e.target,value)}
+                            onChange={(e) => setName(e.target.value)}
                             placeholder="Enter your name"
                         />
                     </label>
