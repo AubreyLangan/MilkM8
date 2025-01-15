3f41f327b74047a76b4551ceb36f8740660c5aad
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2025-01-14 17:26:16 -0800

    Initial Commit | 14 Jan 25 | Reminder Watcher
    Initial coding, to help create the reminder system

---
+++ ReminderWatcher.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useReminders } from "../Contexts/ReminderContext";
+
+const ReminderWatcher = () => {
+    const { reminders } = useReminders();
+
+    useEffect(() => {
+        const checkReminders = () => {
+            const now = new Date();
+            reminders.forEach((reminder) => {
+                const reminderTime = new Date(`${reminder.date}T${reminder.time}`);
+                if (reminderTime <= now) {
+                    alert(`Reminder: ${reminder.title}`);
+                }
+            });
+        };
+
+        const interval = setInterval(checkReminders, 6000);
+        return () => clearInterval(interval);
+    }, [reminders]);
+};
+
+export default ReminderWatcher;
