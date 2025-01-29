883c85ff92f0e62380dc571ee6fd74820d905ff7
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2025-01-29 10:29:32 -0800

    Commit | 29 Jan 25 | Confirmation Modal
    Fixes made to prevent the Confirmation Modal from breaking the app

--- ConfirmatioModal.js
+++ ConfirmatioModal.js
@@ -1,13 +1,19 @@
 import React from "react";
 
-const ConfirmationModal = ({ message, onConfirm, onCancel }) => (
-    <div className="modal-overlay">
-        <div className="modal">
-            <h3>{message}</h3>
-            <button onClick={onConfirm}>Yes, Delete</button>
-            <button onClick={onCancel}>Cancel</button>
+const ConfirmationModal = ({ isOpen, message, onConfirm, onCancel }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div className="modal-overlay">
+            <div className="modal-content">
+                <p>{message}</p>
+                <div>
+                    <button onClick={onConfirm}>Yes</button>
+                    <button onClick={onCancel}>Cancel</button>
+                </div>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default ConfirmationModal;
