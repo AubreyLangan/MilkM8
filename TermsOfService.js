894633aca2f58edce6982b6cd3772334afae52d4
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2025-03-10 17:54:55 -0700

    Initial Commit | 10 Mar 25 | ToS
    Initial code for the new Terms of Service page

---
+++ TermsOfService.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const TermsOfService = () => {
+    return (
+        <div className="terms-container">
+            <h1>Terms of Service</h1>
+        </div>
+    )
+}
