81014c6151e0d71231d0186bd66b5e4b4ec1c4b9
AubreyLangan <141950977+AubreyLangan@users.noreply.github.com>
2025-03-12 11:31:10 -0700

    Commit | 12 Mar 25 | Footer
    Small change made to improve overall look of the Footer component

--- Footer.js
+++ Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
                 <nav className="footer-nav">
                     <a href="/privacy-policy">Privacy Policy</a>
                     <a href="/terms-of-service">Terms of Service</a>
-                    <a href="/contact-page">Contact</a>
+                    <a href="/contact-page">Contact Us</a>
                 </nav>
             </div>
         </footer>
